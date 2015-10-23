1f5c519b9de9c1cf2a9dc5d8a5551b20ad27ff5c
anuragmeena92 <anuragmeena92@gmail.com>
2015-10-23 13:07:48 +0530

    minor

--- app/admin.py
+++ app/admin.py
@@ -5,7 +5,7 @@ class OutletAdmin(admin.ModelAdmin):
     list_display = ('full_name','category','phone_no_primary',)
     list_filter = ('category','location','wifi')
 
-admin.site.register(Outlet)
+admin.site.register(Outlet,OutletAdmin)
 admin.site.register(Location)
 admin.site.register(Wifi_Details)
 admin.site.register(UserProfile)
