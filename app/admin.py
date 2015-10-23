30af415c8bca03fa0db618c6b7bbc9622d0a79c2
anuragmeena92 <anuragmeena92@gmail.com>
2015-10-23 13:04:06 +0530

    minor

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from app.models import Outlet, Wifi_Details, Location, UserProfile
 
+class OutletAdmin(admin.ModelAdmin):
+    list_display = ('full_name','category','phone_no_primary',)
+    list_filter = ('category','location','wifi')
+
 admin.site.register(Outlet)
 admin.site.register(Location)
 admin.site.register(Wifi_Details)
