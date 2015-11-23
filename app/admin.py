704a0618762119ea51eedb45e1ce0151ed9abb22
anuragmeena92 <anuragmeena92@gmail.com>
2015-11-23 14:49:05 +0530

    minor;

--- app/admin.py
+++ app/admin.py
@@ -9,9 +9,11 @@ class CatAdmin(admin.ModelAdmin):
     list_display = ('name','is_active','display_order',)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('outlet','rating',)
+class SuggestionAdmin(admin.ModelAdmin):
+    list_display = ('name','city',)
 admin.site.register(Outlet,OutletAdmin)
 admin.site.register(Location)
 admin.site.register(Category,CatAdmin)
 admin.site.register(UserProfile)
-admin.site.register(Suggestions)
+admin.site.register(Suggestions,SuggestionAdmin)
 admin.site.register(Review,ReviewAdmin)
